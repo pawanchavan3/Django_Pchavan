6ef729bb3b9a50b0543d02066f3b1634381a6a3f
pawanchavan3 <143398041+pawanchavan3@users.noreply.github.com>
2023-08-31 16:20:48 +0530

    commit:5,6

---
+++ mysite/food/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path,include
+from food import views
+
+urlpatterns = [
+    path('home/',views.index,name='index'),
+    path('detail/',views.detail,name='detail')
+]
--- mysite/food/views.py
+++ mysite/food/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse('<h1 style="color:black">This is an index views</h1>')
+
+def detail(request):
+    return HttpResponse('<h1 style="color:blue">This is an details</h1>')
