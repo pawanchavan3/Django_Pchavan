21050a96c1bd833107ec120d5ee7eb2c3f4b0170
pawanchavan3 <143398041+pawanchavan3@users.noreply.github.com>
2023-09-04 15:35:12 +0530

    commit:7

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -5,5 +5,4 @@ from django.db import models
 class item(models.Model):
     item_name=models.CharField(max_length=50)
     item_desc=models.CharField(max_length=300)
-    item_price=models.ImageField()
-    
+    item_price=models.IntegerField()
