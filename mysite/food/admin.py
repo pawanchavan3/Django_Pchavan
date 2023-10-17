801410fa6f44ff9fdc34198c26960a3613cf1266
pawanchavan3 <143398041+pawanchavan3@users.noreply.github.com>
2023-10-17 15:42:54 +0530

    commit:70

--- mysite/food/admin.py
+++ mysite/food/admin.py
@@ -1,5 +1,6 @@
 from django. contrib import admin 
-from food.models import Item
+from food.models import Item, History
 
 # Register your models here.
 admin.site.register(Item)
+admin.site.register(History)
